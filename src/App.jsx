cb42288b93e1dab1a08014af59bd0e8de1ea5c4e
eric <ericwang42@outlook.com>
2022-07-25 22:29:29 -0700

    update?

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
+import { useState } from "react";
 import "./App.css";
-import data from "./data/data";
-import Card from "./components/card";
+import nav from "./components/nav";
 
 function App() {
   return (
-    <div>
-      <Card question={data.q1} answer={data.a1} />
-      <Card question={data.q2} answer={data.a2} />
-      <Card question={data.q3} answer={data.a3} />
-      <Card question={data.q4} answer={data.a4} />
-    </div>
+    <main>
+      <nav />
+    </main>
   );
 }
 
